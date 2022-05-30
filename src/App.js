0ecefd4eb4ecd42af56ac10846af6a801e2b4758
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-05-31 03:36:46 +0530

    added froend ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react"
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route  } from "react-router-dom"
+import store from '../src/redux/store';
+import Greeting from "../src/component/Greeting";
+import Header from "../src/component/Header";
+import Home from "../src/component/Home";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+    <Route
+          path="/"
+          element={(
+            <Home />
+        )}
+        />
+    <Route
+          path="/greeting"
+          element={(
+            <Greeting />
+        )}
+        />
+    </Routes>
+    </BrowserRouter>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
