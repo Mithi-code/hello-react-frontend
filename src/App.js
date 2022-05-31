6a410ad094f249f0fcac559b8f0395491bed0746
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-05-31 19:49:42 +0530

    fixed linter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import store from '../src/redux/store';
-import Greeting from '../src/component/Greeting';
-import Header from '../src/component/Header';
-import Home from '../src/component/Home';
+import store from './redux/store';
+import Greeting from './component/Greeting';
+import Header from './component/Header';
+import Home from './component/Home';
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/greeting' element={<Greeting />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/greeting" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
--- src/component/Greeting.js
+++ src/component/Greeting.js
@@ -13,11 +13,12 @@ const Greeting = () => {
     window.location.reload(false);
   };
   return (
-    <div className='mt-20 flex flex-col items-center'>
-      <p className=' font-bold'>{greeting}</p>
+    <div className="mt-20 flex flex-col items-center">
+      <p className="font-bold">{greeting}</p>
       <button
+        type="button"
         onClick={refreshPage}
-        className='bg-sky-500 p-2 mt-4 rounded text-slate-50'
+        className="bg-sky-500 p-2 mt-4 rounded text-slate-50"
       >
         Get another greeting
       </button>
